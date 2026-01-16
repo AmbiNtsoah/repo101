cd337df5a4ce66d7d079446207621a41e3e611c2
AmbiNtsoah <ambinints04@gmail.com>
2026-01-16 14:31:13 +0400

    Initial Commit

---
+++ main.py
@@ -0,0 +1,5 @@
+# def print_greet(name):
+#     print(f'Welcome {name} !')
+#
+# if __name__ == '__main__':
+#     print_greet('Hary')
---
+++ variables.py
@@ -0,0 +1,25 @@
+""" This file contains variables """
+name = 'Maminirina' #string
+print(type(name))
+
+age = 21
+print(type(age)) #int
+
+weight = 60.3
+print(type(weight)) #float
+
+hobbies = ['Basketball', 'Video Games', 'Running', 'Cinema', 'Walking'] #list
+ranges= len(hobbies)
+for element in range(ranges):
+    print(type(hobbies))
+    print(element[ranges])
+
+my_top_5 = {
+    'movie': 'Entergalactic',
+    'anime': 'Naruto',
+    'activity': 'Basketball',
+    'meal': 'riz cantonnais',
+    'shoes': 'AJ1 Travis Scott'
+}
+print(type(my_top_5))
+
